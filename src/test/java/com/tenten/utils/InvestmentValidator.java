e418f85e20f72e228d723f47a61cdcddfb4e2ee8
JohnTSalmon <104514061+JohnTSalmon@users.noreply.github.com>
2025-09-20 12:42:21 +0100

    Add files via upload

--- src/test/java/com/tenten/utils/InvestmentValidator.java
+++ src/test/java/com/tenten/utils/InvestmentValidator.java
@@ -1,1 +1,23 @@
+package com.tenten.utils;
 
+import com.tenten.dto.Investment;
+
+public class InvestmentValidator {
+
+    public boolean validate(Float calculated, Investment investment) {
+       // Validate the result string against the investment object
+       Float initial = investment.getInitial();
+       Float interestRate =investment.getInterestRate();
+       Integer duration = investment.getDuration();
+       int frequency = investment.getFrequency().getValue();
+       // A = P(1 + r/n)nt
+        Float total = (float) (initial * Math.pow((1 + (interestRate / 100) / frequency), frequency * duration));
+        return calculated.equals(total);
+    }
+}
+// total = P(1 + r/n)nt
+// total = final amount
+// P = initial principal balance
+// r = interest rate
+// n = number of times interest is applied per time period
+// t = number of time periods elapsed
