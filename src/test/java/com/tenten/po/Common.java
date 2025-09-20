26e005d5ebac3a6b2e042bb52b2710d2a8828ef4
JohnTSalmon <104514061+JohnTSalmon@users.noreply.github.com>
2025-09-20 12:50:46 +0100

    Add files via upload

--- src/test/java/com/tenten/po/Common.java
+++ src/test/java/com/tenten/po/Common.java
@@ -1,1 +1,11 @@
+package com.tenten.po;
 
+import com.tenten.CommonPageObject;
+import com.tenten.Enums;
+
+public class Common extends CommonPageObject {
+
+    public void openBrowser(String browser) {
+        open(Enums.Browser.valueOf(browser));
+    }
+}
