5137c0cd430697d23b45fe79af9c3e76e5b0bf56
JohnTSalmon <104514061+JohnTSalmon@users.noreply.github.com>
2025-09-20 12:37:45 +0100

    Add files via upload

---
+++ src/test/java/com/tenten/dto/DTOMap.java
@@ -0,0 +1,19 @@
+package com.tenten.dto;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class DTOMap {
+
+    private Map<String, String> map;
+
+    public DTOMap(Map map) {
+        this.map = map;
+    }
+    
+    public String get(String key){
+        String value;
+        value = map.get(key);
+        return Objects.requireNonNullElse(value, "");
+    }
+}
--- src/test/java/com/tenten/dto/Investment.java
+++ src/test/java/com/tenten/dto/Investment.java
@@ -1,1 +1,48 @@
+package com.tenten.dto;
 
+import com.tenten.CPOException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Investment {
+
+    public enum Frequency {
+        YEARLY(1),
+        QUARTERLY(4),
+        MONTHLY(12),
+        DAILY(365);
+
+        private final int value;
+
+        Frequency(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    private Float initial;
+    private Float interestRate;
+    private Integer duration;  // Years
+    private Float contribution;
+    private Frequency frequency;
+
+
+    public static Investment decode(DTOMap row) {
+        try {
+            return new Investment(
+                    Float.valueOf(row.get("initial")),
+                    Float.valueOf(row.get("interestRate")),
+                    Integer.valueOf(row.get("duration")),
+                    Float.valueOf(row.get("contribution")),
+                    Frequency.valueOf(row.get("frequency")));
+        }
+        catch (NumberFormatException nfe) {
+            throw new CPOException("Invalid Feature Parameters");
+        }
+    }
+}
